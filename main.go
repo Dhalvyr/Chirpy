60013da6d7b137142e9c99b9f93a876883729074
Allaren <diegogermeijs@gmail.com>
2026-07-30 21:19:54 +0200

    B. Added a readiness endpoint to the server, allowing to quickly check it's status.

--- main.go
+++ main.go
@@ -8,10 +8,17 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 	server := &http.Server{
 	Addr: ":8080",
 	Handler: mux,
 	}
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(response http.ResponseWriter, req *http.Request) {
+	response.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	response.WriteHeader(http.StatusOK)
+	response.Write([]byte("OK"))
 }

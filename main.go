9a9aa1b19279bc4471fec0f004d8c74b799044ae
Allaren <diegogermeijs@gmail.com>
2026-08-07 23:18:08 +0200

    F. Started sql and goose implementation.

--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	_ "github.com/lib/pq"
+	"github.com/joho/godotenv"
+	"github.com/Dhalvyr/Chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbQueries := database.New(db)
 	mux := http.NewServeMux()
 	
-	apiCfg := apiConfig{}
+	apiCfg := apiConfig{db: dbQueries}
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)

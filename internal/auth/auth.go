f87b2207f209c6b7718e25827ccefec8e8110179
Allaren <diegogermeijs@gmail.com>
2026-08-18 21:25:21 +0200

    L. Added first functions to implement jwt security measures.

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,11 @@
 package auth
 
 import (
+	"time"
+	
 	"github.com/alexedwards/argon2id"
+	"github.com/google/uuid"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func HashPassword(password string) (string, error) {
@@ -18,4 +22,47 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 		return false, err
 	}
 	return match, nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error){
+	claims := jwt.RegisteredClaims{
+		Issuer: "chirpy-access",
+		IssuedAt: jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject: userID.String(),
+	}
+
+	newToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedString, err := newToken.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+
+	return signedString, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	parsedToken, err := jwt.ParseWithClaims(
+		tokenString, 
+		&claims, 
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(tokenSecret), nil
+		},
+	)
+	if err != nil{
+		return uuid.Nil, err
+	}
+
+	id, err := parsedToken.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	parsedID, err := uuid.Parse(id)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return parsedID, nil
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,6 +1,11 @@
 package auth
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 func TestCheckPasswordHash(t *testing.T) {
 	password := "12345"
@@ -16,5 +21,52 @@ func TestCheckPasswordHash(t *testing.T) {
 
 	if check != true {
 		t.Errorf("Hashed passord and original password do not match")
+	}
+}
+
+func TestJWT(t *testing.T) {
+	id := uuid.New()
+	secretString := "secretString1"
+	tokenString, err := MakeJWT(id, secretString, 15 * time.Minute)
+	if err != nil {
+		t.Errorf("%v\n", err)
+	}
+
+	returnedID, err := ValidateJWT(tokenString, secretString)
+	if err != nil {
+		t.Errorf("%v\n", err)
+	}
+
+	if id != returnedID {
+		t.Errorf("validated id does not match original id")
+	}
+}
+
+func TestJWTtimedout(t *testing.T) {
+	id := uuid.New()
+	secretString := "secretString1"
+	tokenString, err := MakeJWT(id, secretString, -1 * time.Second)
+	if err != nil {
+		t.Errorf("%v\n", err)
+	}
+
+	_, err = ValidateJWT(tokenString, secretString)
+	if err == nil {
+		t.Errorf("expected expired token error, got none")
+	}
+}
+
+func TestJWTWrongSecret(t *testing.T) {
+	id := uuid.New()
+	secretString1 := "secretString1"
+	secretString2 := "secretString2"
+	tokenString, err := MakeJWT(id, secretString1, 15 * time.Minute)
+	if err != nil {
+		t.Errorf("%v\n", err)
+	}
+
+	_, err = ValidateJWT(tokenString, secretString2)
+	if err == nil {
+		t.Errorf("expected error due to mismatching secret token, got none")
 	}
 }
